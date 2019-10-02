49eb2258f2a962506c2bbd391942105324445db0
Dave <macharia3041@gmail.com>
2019-10-03 00:17:51 +0300

    integrated swagger

--- api/urls.py
+++ api/urls.py
@@ -7,9 +7,11 @@ from rest_framework import routers
 from django.urls import path, include
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
 
 from .v1.views.user_views import UserViewSet
 
+schema_view = get_swagger_view(title='Pastebin API')
 
 router = routers.DefaultRouter()  # Defines all the standard REST methods
 router.register(r'users', UserViewSet)  # registers the user view
@@ -17,5 +19,6 @@ router.register(r'users', UserViewSet)  # registers the user view
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
     path('users', UserViewSet.as_view({'get': 'list'}), name="users"),
+    url(r'^$', schema_view),
     path('', include(router.urls)),
 ]
